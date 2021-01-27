5ec8c225a1f97501a74602b3f282f90ba04e8158
Volkov R.O <blattidaer@gmail.com>
2021-01-27 17:38:36 +0300

    add func for GET request

--- httpreq/httpreq.go
+++ httpreq/httpreq.go
@@ -40,3 +40,38 @@ func PostReq(data map[string]interface{}, url string, headers ...map[string]stri
 
 	return result, nil
 }
+
+// GetReq - Use for GET request
+func GetReq(data map[string]interface{}, url string, headers ...map[string]string) (response map[string]interface{}, err error) {
+
+	reqBody, err := json.Marshal(&data)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("GET", url, bytes.NewBuffer(reqBody))
+	if err != nil {
+		return nil, err
+	}
+
+	if len(headers) != 0 {
+		for i := 0; i < len(headers); i++ {
+			for s, e := range headers[i] {
+				req.Header.Add(s, e)
+			}
+		}
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	var result map[string]interface{}
+	json.NewDecoder(resp.Body).Decode(&result)
+
+	return result, nil
+}
